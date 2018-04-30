6f42eca8d33a68ca208d7fe38165dc19328af061
chaibin <cbb_zxy@163.com>
2018-04-30 16:30:43 +0800

    first commit

---
+++ take_a_break.py
@@ -0,0 +1,11 @@
+# _*_ coding:unicode _*_
+import time
+import webbrowser
+total_breaks = 4
+break_count = 0
+print("������{0}��ʼ���".format(time.ctime()))
+while (break_count < total_breaks):
+    time.sleep( 10)
+    webbrowser.open("http://music.163.com/#/discover/toplist?id=19723756")
+    break_count = break_count + 1
+print("������{0}����������Ϣ��{1}�Σ�����".format(time.ctime(),total_breaks))
